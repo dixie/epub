e5f1a9a097f478f7950aa34ed58c8117b371c77a
dixiecko <dixiecko@gmail.com>
2009-12-29 01:01:33 +0000

    Partly implemented container construction.

--- src/Codec/Archive/OCF.hs
+++ src/Codec/Archive/OCF.hs
@@ -1,16 +1,59 @@
-module OCF where
+module OCF(
+	OCF(..),
+	RootItem(..),
+	defaultMimetype,
+	emptyOCF,
+	addRootItem,
+	addFile
+       )
+where
 
+import Text.XML.Light
 import Codec.Archive.Zip
+import qualified Data.ByteString.Lazy as B
+
+defaultMediatype = "application/oebps-package+xml"
+defaultMimetype = "application/epub+zip"
 
 -- | Represents OCF Container Format
-data OCF = OCF { ocMetadata :: String,
-                 ocRootfiles :: [RootFile],
-                 ocFiles :: [Files] } deriving Show
+data OCF = OCF { ocfMimetype  :: String,
+                 ocfRootItems :: [RootItem],
+                 ocfEntries   :: [Entry] } deriving Show
 
 -- | Represent one root file within OCF
-data RootFile = RootFile { rfPath :: String, rfMediaType :: String } deriving Show
+data RootItem = RootItem { rfPath :: String, rfMediaType :: String } deriving Show
 
--- | Create ZIP archive with represents OCF
+-- | Create empty OCF Container with defaultMimetype
+emptyOCF :: OCF
+emptyOCF = OCF defaultMimetype [] []
 
-writeOCF :: FilePath -> OCF -> IO ()
-writeOCF f o = do
+-- | Add Root Item into OCF
+addRootItem :: OCF -> RootItem -> OCF
+addRootItem o i = o { ocfRootItems = its } 
+	where
+	   its = i:(ocfRootItems o) 
+
+-- | Put file within container
+addFile :: OCF -> FilePath -> B.ByteString -> OCF
+addFile o f c = o { ocfEntries = ets }
+       where
+           ets = (toEntry f 0 c):(ocfEntries o)
+
+{-
+<?xml version="1.0"?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+   <rootfiles>
+      <rootfile full-path="content.opf" media-type="application/oebps-package+xml"/>
+   </rootfiles>
+</container>
+-}
+mkContainerXML :: OCF -> String
+mkContainerXML o = ppTopElement contTag
+	where
+	   contTag = add_attrs contAttrs $ unode "container" rootfilesTag
+           contAttrs = [ (Attr (unqual "version") "1.0")
+			,(Attr (unqual "xmlns")   "urn:oasis:names:tc:opendocument:xmlns:container") ] 
+           rootfilesTag = unode "rootfiles" (map rootfileTag (ocfRootItems o))
+           rootfileTag (RootItem p m) = add_attrs (rfAttrs p m) $ unode "rootfile" ()
+           rfAttrs p m = [ (Attr (unqual "full-path") p)
+			  ,(Attr (unqual "media-type")  m) ] 
