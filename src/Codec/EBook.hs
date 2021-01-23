79c02ce93d2d6a31cb44646380442291387eef53
dixie <dixie@rampa.sk>
2021-01-23 17:45:30 +0000

    fix comments.

--- src/Codec/EBook.hs
+++ src/Codec/EBook.hs
@@ -19,27 +19,27 @@ import Codec.EBook.OPF
 import Codec.EBook.OCF
 import qualified Data.ByteString.Lazy as B
 
--- EPUB media type
+-- | EPUB media type
 opsMediatype :: String
 opsMediatype = "application/xhtml+xml"
 
--- Serialize Book to binary EPUB Format
+-- | Serialize Book to binary EPUB Format
 book2Bin :: Book -> Integer -> B.ByteString
 book2Bin book = fromArchive . book2Arch book
 
--- Serialize Book to binary EPUB Format with current time
+-- | Serialize Book to binary EPUB Format with current time
 book2Bin' :: Book -> IO B.ByteString
 book2Bin' book = do 
           (TOD t _) <- getClockTime 
           return $ fromArchive (book2Arch book t)
 
--- Serialize Book to ZIP Archive with current time
+-- | Serialize Book to ZIP Archive with current time
 book2Arch' :: Book -> IO Archive
 book2Arch' book = do
           (TOD t _) <- getClockTime 
           return $ book2Arch book t
 
--- Serialize Book to ZIP Archive
+-- | Serialize Book to ZIP Archive
 book2Arch :: Book -> Integer -> Archive
 book2Arch book t = let conXMLFile = containerXMLFile' opfFileName
                        opfFileName = "book.opf"
